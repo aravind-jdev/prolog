7dd8a852e2791cce93cda911c322a00e5b0ea50a
aravind-jdev <aravind18l15@gmail.com>
2026-02-10 11:23:56 +0530

    n-queen new code

--- roku.pl
+++ roku.pl
@@ -1,14 +1,16 @@
-solution([]).
-solution([X/Y | Others]):-
-    solution(Others),
-    mem(Y,[1,2,3,4,5,6,7,8]),
-    noattack(X/Y,Others),
-noattack(_,[]).
-noattack(X/Y,[X1/Y1 | Others]):-
- Y=\=Y1,
- Y1-Y=\=X1-X,
- Y1-Y=\=X-X1,
- noattack(X/Y,Others).
-mem(X,[X|_]).
-mem(X,[_|T]) :- mem(X,T).
-template([1/Y1,2/Y2,3/Y3,4/Y4,5/Y5,6/Y6,7/Y7,8/Y8]).
+n_queens(N, Qs) :-
+        length(Qs, N),
+        Qs ins 1..N,
+        safe_queens(Qs).
+
+safe_queens([]).
+safe_queens([Q|Qs]) :-
+        safe_queens(Qs, Q, 1),
+        safe_queens(Qs).
+
+safe_queens([], _, _).
+safe_queens([Q|Qs], Q0, D0) :-
+        Q0 #\= Q,
+        abs(Q0 - Q) #\= D0,
+        D1 #= D0 + 1,
+        safe_queens(Qs, Q0, D1).
